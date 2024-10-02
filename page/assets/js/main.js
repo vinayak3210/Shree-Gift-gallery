0cea0e2e1a6195a838aa4a049a881758c7f63017
vinayak3210 <kumrawatvinayak@gmail.com>
2024-10-02 17:01:23 +0530

    vinayak

---
+++ page/assets/js/main.js
@@ -0,0 +1,20 @@
+
+
+const correctUsername = localStorage.getItem('username');
+const correctPassword = localStorage.getItem('password');
+
+document.getElementById("login").addEventListener('click',function(){
+    let UserInput=document.getElementById("username").value;
+    let Password=document.getElementById("pass").value;
+
+    if(UserInput==correctUsername && Password==correctPassword){
+        localStorage.setItem('username', UserInput);
+        window.open('dashboard.html')
+    }
+    else{
+        
+        alert("Incorrect ID Password ");
+        
+    }
+})
+

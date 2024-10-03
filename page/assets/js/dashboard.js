21b4862bedd75e07ae607b86d6d3578706a38e70
vinayak3210 <kumrawatvinayak@gmail.com>
2024-10-03 19:12:27 +0530

    vinayak

--- page/assets/js/dashboard.js
+++ page/assets/js/dashboard.js
@@ -1,34 +1,34 @@
-// const username = localStorage.getItem('username');
+// // const username = localStorage.getItem('username');
 
-// let welcome= document.getElementById("welcome");
-// welcome.textContent=`WelCome to ${username}`;
+// // let welcome= document.getElementById("welcome");
+// // welcome.textContent=`WelCome to ${username}`;
 
 
-// document.getElementById('logout').addEventListener('click', () => {
-//     // Redirect back to the login page (index.html)
-//     localStorage.removeItem('username');
+// // document.getElementById('logout').addEventListener('click', () => {
+// //     // Redirect back to the login page (index.html)
+// //     localStorage.removeItem('username');
 
-//       // Close the dashboard tab and redirect the user to login
-//       const dashboardTab = localStorage.getItem('dashboardTab');
+// //       // Close the dashboard tab and redirect the user to login
+// //       const dashboardTab = localStorage.getItem('dashboardTab');
 
-//       if (dashboardTab && !dashboardTab.closed) {
-//         dashboardTab.close();  // Attempt to close the tab (will work only if opened with window.open)
-//       }
-//       window.location.href = 'Gifts.html'; 
-// })
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-});
+// //       if (dashboardTab && !dashboardTab.closed) {
+// //         dashboardTab.close();  // Attempt to close the tab (will work only if opened with window.open)
+// //       }
+// //       window.location.href = 'Gifts.html'; 
+// // })
+// document.getElementById('registrationForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+// });
 
-const Username = localStorage.getItem('username');
+// const Username = localStorage.getItem('username');
     
-    // Display the welcome message with the username
-    const welcomeMessage = document.getElementById('welcome');
-    welcomeMessage.textContent = `Welcome , ${Username}!`;
+//     // Display the welcome message with the username
+//     const welcomeMessage = document.getElementById('welcome');
+//     welcomeMessage.textContent = `Welcome , ${Username}!`;
 
-    // Logout button logic
-    document.getElementById('logout').addEventListener('click', function() {
-      // Clear the username from localStorage
+//     // Logout button logic
+//     document.getElementById('logout').addEventListener('click', function() {
+//       // Clear the username from localStorage
       
-      window.location.href = 'gifts.html';
-    });
+//       window.location.href = 'gifts.html';
+//     });
--- page/assets/js/main.js
+++ page/assets/js/main.js
@@ -1,5 +1,4 @@
 
-
 const correctUsername = localStorage.getItem('username');
 const correctPassword = localStorage.getItem('password');
 
